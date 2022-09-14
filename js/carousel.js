2d3068ad400981e06f4bb0509dc236ac81bcaa32
eip <36315290+eip618@users.noreply.github.com>
2022-09-15 09:20:55 +1000

    major updates
    -change colours
    -fix card carousels

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,5 @@
 		$('#recipeCarousel').carousel({
-  interval: 10000
+  interval: false
 })
 
 $('.carousel .carousel-item').each(function(){
@@ -19,45 +19,10 @@ $('.carousel .carousel-item').each(function(){
         next.children(':first-child').clone().appendTo($(this));
       }
 });
-		$('#recipeCarousel2').carousel2({
-  interval: 10000
+		$('#recipeCarousel2').carousel({
+  interval: false
 })
 
-$('.carousel2 .carousel-item2').each(function(){
-    var minPerSlide = 3;
-    var next = $(this).next();
-    if (!next.length) {
-    next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-    
-    for (var i=0;i<minPerSlide;i++) {
-        next=next.next();
-        if (!next.length) {
-        	next = $(this).siblings(':first');
-      	}
-        
-        next.children(':first-child').clone().appendTo($(this));
-      }
-});
-		$('#recipeCarousel3').carousel3({
-  interval: 10000
+		$('#recipeCarousel3').carousel({
+  interval: false
 })
-
-$('.carousel3 .carousel-item3').each(function(){
-    var minPerSlide = 3;
-    var next = $(this).next();
-    if (!next.length) {
-    next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-    
-    for (var i=0;i<minPerSlide;i++) {
-        next=next.next();
-        if (!next.length) {
-        	next = $(this).siblings(':first');
-      	}
-        
-        next.children(':first-child').clone().appendTo($(this));
-      }
-});
